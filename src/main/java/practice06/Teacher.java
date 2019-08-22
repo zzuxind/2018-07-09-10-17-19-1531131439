cf5d1383120fc571ffc4c14b38936097e1d633d1
dingxin <zzuxind@163.com>
2019-08-22 21:28:22 +0800

    practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int Klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduceMessage="";
+        if(this.Klass != 0){
+             introduceMessage=super.introduce()+"I am a Teacher. I teach Class "+this.Klass+".";
+        }else {
+            introduceMessage= super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+        return introduceMessage;
+    }
 }
