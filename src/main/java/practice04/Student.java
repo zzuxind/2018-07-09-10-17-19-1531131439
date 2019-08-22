570c51574ea1ed3bf0d73dc54e6d7ae792931ed8
dingxin <zzuxind@163.com>
2019-08-22 20:53:35 +0800

    practice4

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+public class Student extends  Person{
+    private int kClass;
+
+    public int getkClass() {
+        return kClass;
+    }
+
+    public void setkClass(int kClass) {
+        this.kClass = kClass;
+    }
+
+    public Student(String name, int age, int kClass) {
+        super(name, age);
+        this.kClass = kClass;
+    }
+
+    @Override
+    public String introduce() {
+        return this.basicIntroduce()+" I am a Student. I am at Class "+this.kClass+".";
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends  Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return this.basicIntroduce()+" I am a Worker. I have a job.";
+    }
 }
